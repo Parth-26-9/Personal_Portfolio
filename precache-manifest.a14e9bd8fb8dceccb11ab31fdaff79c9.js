68169e6fa88dddd86f2a8c8e812b3cfa3165c036
parth-122 <100356812+parth-122@users.noreply.github.com>
2023-04-03 17:21:25 +0530

    Updates

---
+++ precache-manifest.a14e9bd8fb8dceccb11ab31fdaff79c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "617b75308d9856b63cf3",
+    "url": "/Personal_Portfolio/static/css/main.14adc031.chunk.css"
+  },
+  {
+    "revision": "617b75308d9856b63cf3",
+    "url": "/Personal_Portfolio/static/js/main.617b7530.chunk.js"
+  },
+  {
+    "revision": "03570c0e386926ac9d08",
+    "url": "/Personal_Portfolio/static/js/1.03570c0e.chunk.js"
+  },
+  {
+    "revision": "ad1bb4f6c4c44aaf65d5",
+    "url": "/Personal_Portfolio/static/js/runtime~main.ad1bb4f6.js"
+  },
+  {
+    "revision": "a8d77740fce54f2cbe46a63434092350",
+    "url": "/Personal_Portfolio/index.html"
+  }
+];
